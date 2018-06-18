101226a5b24feb720f9d3fa974820ca8c55ae3f1
user <user@users-MacBook-Pro-2.local>
2018-06-18 20:43:47 +0300

    RESTAAJAX

--- public/js/restaurant/restaurant-repository.js
+++ public/js/restaurant/restaurant-repository.js
@@ -1,2 +1,14 @@
+class RestaurantRepository {
+  constructor() {
+    this.orders = [];
+  }
+
+  addNewOrder(newOrder) {
+    return $.post('/orders', newOrder, (orderReceived)=> {
+      this.orderes.push(orderReceived);
+    } )
+  }
+}
 
 
+export default RestaurantRepository;
