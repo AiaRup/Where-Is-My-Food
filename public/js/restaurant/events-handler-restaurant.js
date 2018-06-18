75acfda193cb533e87a4c58a1811c66ac43c79a6
user <user@users-MacBook-Pro-2.local>
2018-06-18 18:32:24 +0300

    restaurantClient

--- public/js/restaurant/events-handler-restaurant.js
+++ public/js/restaurant/events-handler-restaurant.js
@@ -1,7 +1,53 @@
 class EventsHandler {
-  constructor() {
-
+  constructor(restaurantRepositories, restaurantRenders) {
+    this.restaurantRepositories = restaurantRepositories;
+    this.restaurantRenders = restaurantRenders;
+    this.counter = 0;
   }
+
+  fillNewForm() {
+    $(".btnNewOrder").click((event) => {
+      event.stopPropagation();
+      $('.order-form').toggle();
+    })
+  }
+
+  submitNewOrder() {
+    $(".submitOrder").click((event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      let name = $('#name').val();
+      let location = $('#address').val();
+      let phoneNumber = $('#phoneNumber').val();
+      let paymentMethod = $('#paymentSelection').val();
+      let dishSelection = $('#dishOptions').find(':selected').val();
+
+      //order object
+      let newOrder = {
+        name: name,
+        location: location,
+        phoneNumber: phoneNumber,
+        paymentMethod: paymentMethod,
+        dishes: dishes,
+        totalPrice: totalPrice,
+        status: "received",
+        orderId: orderId,
+        isTaken: false,
+        orderId: counter++
+      }
+
+      this.restaurantRepositories.addNewOrder(newOrder)
+
+      $('.modal').show();
+    })
+  }
+
+  closePopUp() {
+    $('.closeBtn').click(() => {
+      $('.modal').hide();
+    })
+  }
+
 }
 
 
