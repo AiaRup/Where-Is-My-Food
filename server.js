6502b06b25d2b537c95e9e3cb46599db394512c2
Kobi <abirkoby@gmail.com>
2018-06-26 17:48:39 +0300

    changed to port 80

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // PORT
-const SERVER_PORT = process.env.PORT || 8000;
+const SERVER_PORT = process.env.PORT || 80;
 app.listen(SERVER_PORT, () => console.log(`Server up and running on port ${SERVER_PORT}...`));
 
 
